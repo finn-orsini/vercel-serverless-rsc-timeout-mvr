c15ebe54df329a19054c8dd3d12a90743ffecf81
Seraphina Orsini <29002354+finn-orsini@users.noreply.github.com>
2023-09-21 09:29:35 -0400

    chore: comment out static

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -6,11 +6,16 @@ const inter = Inter({ subsets: ['latin'] })
 import {NextIntlClientProvider} from 'next-intl';
 import {notFound} from 'next/navigation';
  
-
+// test with and without this
+// export function generateStaticParams() {
+//   return [{locale: 'en'}];
+// }
+ 
 export const metadata = {
   title: 'MVR',
   description: 'Test Serverless Fn Timeouts',
 }
+
 
 export default async function LocaleLayout({children, params: {locale}}) {
   let messages;
